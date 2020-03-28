ed81e8744983874b669a0d79e50de816a3ef75ab
kokoslik <nnproshunin@gmail.com>
2020-03-28 19:25:35 +0300

    Initial version.

--- travis-test.cpp
+++ travis-test.cpp
@@ -2,7 +2,7 @@
 #include <version.h>
 int main()
 {
-    std::cout<<"Build number "<<BUILD_NUMBER<<std::endl;
+    //std::cout<<"Build number "<<BUILD_NUMBER<<std::endl;
     std::cout<<"Hello world"<<std::endl;
     return 0;
 }
